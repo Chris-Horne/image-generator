6efd6c1669b088590e48936551e82bbc79d884a8
CHRIS HORNE <87401567+Chris-Horne@users.noreply.github.com>
2022-09-15 20:16:10 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const button = document.querySelector("button");
+const authorSpan = document.querySelector(".author");
+const imgDiv = document.querySelector(".image-container");
+const img = document.querySelector(".img");
+
+const getImage = async function() {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    //console.log(images);
+    selectRandomImage(images)
+};
+
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomimage = images[randomIndex];
+    //console.log(randomimage);
+    displayImage(randomimage);
+}
+
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+button.addEventListener("click", function() {
+    getImage();
+});
